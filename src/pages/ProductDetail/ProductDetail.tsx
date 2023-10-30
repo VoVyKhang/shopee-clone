2647ad427d76f8c6bcb056418960b545c945383d
VoVyKhang <vo.vykhang24@gmail.com>
2023-10-30 22:15:13 +0700

    add slider for product images

--- src/pages/ProductDetail/ProductDetail.tsx
+++ src/pages/ProductDetail/ProductDetail.tsx
@@ -1,14 +1,18 @@
 import { useQuery } from 'react-query'
 import { useParams } from 'react-router-dom'
-import productApi from 'src/apis/product.api'
 import { ChevronLeftIcon, ChevronRightIcon, MinusIcon, PlusIcon, ShoppingCartIcon } from 'src/components/Icons'
 import { InputNumber } from 'src/components/InputNumber'
 import { ProductRating } from 'src/components/ProductRating'
 import { formatCurrency, formatNumberToSocialStyle, rateSale } from 'src/utils/utils'
+import { useMemo, useState, useEffect } from 'react'
+import { Product } from 'src/types/product.type'
 import DOMPurify from 'dompurify'
+import productApi from 'src/apis/product.api'
 
 function ProductDetail() {
   const { id } = useParams()
+  const [currentIndexImages, setCurrentIndexImages] = useState([0, 5])
+  const [activeImage, setActiveImage] = useState('')
 
   const { data: productDetailData } = useQuery({
     queryKey: ['product', id],
@@ -18,38 +22,72 @@ function ProductDetail() {
   })
 
   const product = productDetailData?.data.data
+  const currentImages = useMemo(
+    () => (product ? product.images.slice(...currentIndexImages) : []),
+    [product, currentIndexImages]
+  )
+
+  const chooseActive = (img: string) => {
+    setActiveImage(img)
+  }
+
+  const next = () => {
+    if (currentIndexImages[1] < (product as Product)?.images.length) {
+      setCurrentIndexImages((prev) => [prev[0] + 1, prev[1] + 1])
+    }
+  }
+
+  const prev = () => {
+    if (currentIndexImages[0] > 0) {
+      setCurrentIndexImages((prev) => [prev[0] - 1, prev[1] - 1])
+    }
+  }
+
+  useEffect(() => {
+    if (product && product.images.length > 0) {
+      setActiveImage(product.images[0])
+    }
+  }, [product])
+
+  if (!product) return null
 
   return (
     <div className='bg-gray-200 py-6'>
-      <div className='bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='bg-white p-4 shadow'>
           <div className='grid grid-cols-12 gap-9'>
             <div className='col-span-5'>
               <div className='relative w-full pt-[100%] shadow'>
                 <img
                   alt={product?.name}
-                  src={product?.image}
+                  src={activeImage}
                   className='absolute top-0 left-0 h-full w-full bg-white object-cover'
                 />
               </div>
               <div className='relative mt-4 grid grid-cols-5 gap-1'>
-                <button className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'>
+                <button
+                  className='absolute left-0 top-1/2 z-10 h-9 w-5 -translate-y-1/2 bg-black/20 text-white'
+                  onClick={prev}
+                >
                   <ChevronLeftIcon className='w-5 h-5' />
                 </button>
-                {product?.images.slice(0, 5).map((img, index) => {
-                  const isActive = index === 0
+                {currentImages.map((img) => {
+                  const isActive = img === activeImage
                   return (
-                    <div className='relative w-full pt-[100%]' key={img}>
+                    <div className='relative w-full pt-[100%]' key={img} onMouseEnter={() => chooseActive(img)}>
                       <img
                         src={img}
-                        alt={product.name}
+                        alt={product?.name}
                         className='absolute top-0 left-0 w-full h-full cursor-pointer bg-white object-cover'
                       />
                       {isActive && <div className='absolute border-2 border-orange inset-0' />}
                     </div>
                   )
                 })}
-                <button className='absolute right-0 top-1/2 -translate-y-1/2 z-10 h-9 w-5  bg-black/20 text-white'>
+                <button
+                  className='absolute right-0 top-1/2 -translate-y-1/2 z-10 h-9 w-5  bg-black/20 text-white'
+                  onClick={next}
+                >
                   <ChevronRightIcon className='w-5 h-5' />
                 </button>
               </div>
@@ -114,8 +152,8 @@ function ProductDetail() {
         </div>
       </div>
 
-      <div className='mt-8 bg-white p-4 shadow'>
-        <div className='container'>
+      <div className='container'>
+        <div className='mt-8 bg-white p-4 shadow'>
           <div className='rounded bg-gray-50 p-4 text-lg capitalize text-slate-700'>Mô tả sản phẩm</div>
           <div className='mx-4 mt-12 mb-4 text-sm leading-loose'>
             <div
