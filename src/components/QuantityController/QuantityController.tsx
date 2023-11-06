81ee52ddb9a5bb01fc4c02ada7d5632fe7804398
VoVyKhang <vo.vykhang24@gmail.com>
2023-11-06 22:39:18 +0700

    add update inputNumber, QualityController

--- src/components/QuantityController/QuantityController.tsx
+++ src/components/QuantityController/QuantityController.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { MinusIcon, PlusIcon } from '../Icons'
 import { InputNumber, InputNumberProps } from '../InputNumber'
 
@@ -18,6 +19,8 @@ function QuantityController({
   value,
   ...rest
 }: Props) {
+  const [localValue, setLocalValue] = useState<number>(Number(value) || 0)
+
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     let _value = Number(event.target.value)
     if (max !== undefined && _value > max) {
@@ -27,24 +30,27 @@ function QuantityController({
     }
 
     onType && onType(_value)
+    setLocalValue(_value)
   }
 
   const increase = () => {
-    let _value = Number(value) + 1
+    let _value = Number(value || localValue) + 1
     if (max !== undefined && _value > max) {
       _value = max
     }
 
     onIncrease && onIncrease(_value)
+    setLocalValue(_value)
   }
 
   const decrease = () => {
-    let _value = Number(value) - 1
+    let _value = Number(value || localValue) - 1
     if (_value < 1) {
       _value = 1
     }
 
     onDecrease && onDecrease(_value)
+    setLocalValue(_value)
   }
 
   return (
@@ -56,7 +62,7 @@ function QuantityController({
         <MinusIcon className='w-4 h-4' />
       </button>
       <InputNumber
-        value={value}
+        value={value || localValue}
         className=''
         classNameInput='h-8 w-14 border-t border-b border-gray-300 p-1 text-center outline-none'
         classNameError='hidden'
